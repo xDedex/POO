dde9de1c3184d31bac7cc8703b17326c38279f00
Dedê <47751527+srtadede@users.noreply.github.com>
2019-08-26 15:40:52 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+package principal {
+
+import atividade.Conta;
+
+
+public static void main(String[] args) {
+Conta co1 = new Conta();
+Conta co2 = new Conta();
+
+co1.agencia = "1234-5";
+co1.numConta= "258";
+co1.saldo= 23890.45
+co1.debito= 12.60;
+co1.data= "26/08/2019";
+
+co2.agencia = "5432-1";
+co2.numConta= "123";
+co2.saldo= 421.82;
+co2.debito= 18.50;
+co2.data= "26/08/2019";
+
+
+ System.out.println (co1.Conta);
+ System.out.println (co2.Conta);
+
+
+
+}
+
+}
