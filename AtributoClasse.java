d64b19ce5e5263b066a2ff2bda7a3df8f265b1bf
Dedê <47751527+srtadede@users.noreply.github.com>
2019-08-26 15:28:52 -0300

    Create AtributoClasse.java

---
+++ AtributoClasse.java
@@ -0,0 +1,38 @@
+package atividade {
+
+public class  Conta {
+    public static String agencia;
+    public static String numConta;
+    public static double saldo;
+    public static double debito;
+    public static String data;
+    
+}
+
+public class Email {
+    public static String remetente;
+    public static String destinatario;
+    public static String assunto;
+    public static String spam;
+    public static String arquivados;
+    
+}
+
+public class Endereco {
+    public static String rua;
+    public static String bairro;
+    public static String cep;
+    public static int numero;
+    public static String complemento;
+    
+}
+
+public class Telefone {
+    public static String regiao;
+    public static int ddd;
+    public static int operadora;
+    public static int prefixo;
+    public static int numero;
+    
+}
+}
